f2c12e8004fe3c72a9832ce6419dc75ba721b9f2
René van Mil <rvanmil@me.com>
2020-03-09 17:27:17 +0100

    updated loop rules

--- index.js
+++ index.js
@@ -47,8 +47,24 @@ module.exports = {
 			}
 		}],
 		'max-len': 'off',
+		'no-await-in-loop': 'off',
 		'no-new': 'off',
 		'no-param-reassign': ['error', { props: false }],
+		'no-restricted-syntax': [
+			'error',
+			{
+				selector: 'ForInStatement',
+				message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.'
+			},
+			{
+				selector: 'LabeledStatement',
+				message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.'
+			},
+			{
+				selector: 'WithStatement',
+				message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.'
+			}
+		],
 		'no-tabs': 'off',
 		'no-underscore-dangle': ['error', { allow: ['_id'] }],
 		'object-curly-newline': 'off',
